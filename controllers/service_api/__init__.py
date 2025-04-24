9abdc9da5fe713e8f4fc897010c8ea8a2c2a6983
csy100 <csiyu100@gmail.com>
2025-04-24 12:49:19 +0800

    feat:add controller

--- controllers/service_api/__init__.py
+++ controllers/service_api/__init__.py
@@ -6,4 +6,4 @@ from libs.external_api import ExternalApi
 bp = Blueprint("service_api", __name__, url_prefix="/v1")
 api = ExternalApi(bp)
 
-from .app import vote
+from .app import document
---
+++ controllers/service_api/app/document.py
@@ -0,0 +1,6 @@
+class Document:
+    def __init__(self):
+        pass
+
+    def get_document(self):
+        pass
